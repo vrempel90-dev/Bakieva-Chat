3422485bcafdc55e765bdd149637737355569355
vrempel90-dev <vrempel90@gmail.com>
2026-09-21 13:29:54 +0500

    Store user language preference

---
+++ migrations/004_user_language.sql
@@ -0,0 +1,15 @@
+ALTER TABLE users
+  ADD COLUMN IF NOT EXISTS language TEXT;
+
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1
+    FROM pg_constraint
+    WHERE conname = 'users_language_check'
+  ) THEN
+    ALTER TABLE users
+      ADD CONSTRAINT users_language_check
+      CHECK (language IN ('ru','kk'));
+  END IF;
+END $$;
